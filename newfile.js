372d350de612f1175bca34150f968782dae883bf
Prabhata Ranjan Behera <prabhata3593@gmail.com>
2026-01-06 22:23:14 +0530

    changes added in newsfile.js

--- newfile.js
+++ newfile.js
@@ -1,1 +1,48 @@
-"console.log('Hello World');" 
+"console.log('Self introduction:
+
+my name is Prabhata rannjan Behera
+i am from odisha.
+i have completed my bacholer degree in BCA from berhampur university in 2023.
+
+my skills are
+Programing: Python, Advance SQL, T-SQL
+Database:   SQL Server, Oracle SQL, Data Warehousing
+Cloud:      AWS (EC2, ECR, S3, IAM), Azure, Azure Data Factory, Azure Databricks, ADLS Gen2
+Devops:     Git, GitHub Actions, Azure DevOps, CI/CD
+O.S:        Linux, windows
+
+
+PROJECT: 1
+Nginx Application deployment using AWS & CI/CD (Devops Engineer)
+Description:
+Designed and implemented an end-to-end CI/CD pipeline to deploy a Nginx application on AWS
+using Docker and GitHub Actions.
+Key Responsibilities:
+Built and managed a GitHub repository and implemented CI/CD pipelines using GitHub Actions.
+Provisioned an AWS EC2 (Ubuntu) instance and configured IAM for secure access.
+Containerized a Nginx application using Docker and stored images in Amazon ECR.
+Automated build and deployment using YAML workflows and Docker files.
+Successfully deployed and verified the application using the EC2 public IP address.
+Technologies Used:
+Linux, Git, GitHub, GitHub Actions, AWS (EC2, IAM, ECR), Docker, Networking, Nginx
+
+
+PROJECT: 2
+Employee Payroll Automation (Data Engineering)
+Description:
+Designed and implemented an end-to-end data engineering pipeline to automate employee payroll
+processing using Azure Data services.
+Key Responsibilities:
+Built end-to-end payroll data pipelines using CSV source files and Azure Data Factory.
+Processed and transformed data in Azure Databricks using PySpark and SQL to create staging,
+dimension, and fact tables.
+Implemented payroll calculation and data validation logic based on role-wise hourly rates.
+Version-controlled Databricks code using Git and automated deployments with Azure DevOps
+CI/CD pipelines.
+Stored curated Gold-layer data in ADLS Gen2 and created Power BI reports for payroll analysis.
+Technologies Used:
+Azure Data Factory, Databricks, ADLS Gen2, SQL, Python, Azure DevOps, Git, CI/CD, Power BI
+
+PROJECT:3
+Sales database management system (Sql Developer)
+Description:');"
